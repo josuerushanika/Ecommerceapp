bcfc10a118b6ef4c543242c9d703532a2bd8b5cf
josuerushanika <josuerushanika@gmail.com>
2023-07-29 09:11:57 +0200

    Update files

---
+++ estore/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <header>
+        <div className="container-fluid">
+          <div className="row">
+             <div className="col-xs-3 col-lg-2">
+                <div className="header__logo">
+                   <font>eStore</font>
+                </div>
+             </div>
+          </div>
+        </div>
+      </header>
+   
+  );
+}
+
+export default App;
